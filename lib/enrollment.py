5257e4564242aadff8b6d4c5e41dfccb8e99001c
Ronnie Langat <ronnielangat2020@gmail.com>
2024-03-18 10:18:31 +0300

    m

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
@@ -17,7 +18,6 @@ class Student:
 
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -29,7 +29,6 @@ class Course:
 
     def get_enrollments(self):
         return self._enrollments.copy()
-
 
 class Enrollment:
     all = []
@@ -45,3 +44,26 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    def course_count(self):
+        return len(self._enrollments)
+     
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
+    
+    def aggregate_average_grade(self):
+        # lets assume the grades are stored in a protected attribute called _grades. 
+        total_grades = sum(self._grades.values())
+        num_courses = len(self._grades)
+        average_grade = total_grades / num_courses
+
+        return average_grade
+
+
+
+
